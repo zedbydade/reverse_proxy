864aff679858d1b5a4c4a1a761b910469e938c5c
Francisco Paradela <franciscoleite.dev@protonmail.com>
2023-10-20 23:33:56 -0300

    feat: TLS WIP

--- lib/tls_stream.rb
+++ lib/tls_stream.rb
@@ -1,16 +1,20 @@
-class TlsStream 
-  # stream; TCP connection 
-  # config; keys, certifications and TLS security things
+require 'openssl'
+
+class TlsStream
+  # stream; TCP connection
+  # config; keys, certifications and TLS security things(SSLContext)
   attr_accessor :stream, :config
 
   def initialize(stream:, config:)
-    @stream = stream 
+    @stream = stream
     @config = config
   end
 
-  def call 
-    remote_addr = stream.remote_host 
-    accept = OpenSSL::SSL::SSLSocket.new(stream, config)
+  def call
+    remote_addr = stream.remote_host
+    # WARNING
+    # SSL_CONTEXT WILL BE FRONZEN HERE
+    accept = OpenSSL::SSL::SSLSocket.new(stream, config).connect
     { remote_addr:, accept: }
-  end 
+  end
 end
