9162ec7a4b5d78124ff9a72b5779f9e28b679975
Francisco Paradela <franciscoleite.dev@protonmail.com>
2023-11-07 19:34:54 -0300

    chore: Add tcp_server

--- utils/tcp_server.rb
+++ utils/tcp_server.rb
@@ -2,10 +2,11 @@
 
 require 'socket'
 
-server = TCPServer.new 2000 # Server bind to port 2000
+server = TCPServer.new('localhost', 2000) # Server bind to port 2000
 loop do
-  client = server.accept    # Wait for a client to connect
-  client.puts "TU-TU-RU"
+  p "Starting server: #{server.inspect}"
+  client = server.accept # Wait for a client to connect
+  p "With client: #{client.inspect}"
+  client.write 'TU-TU-RU'
   client.close
 end
-
