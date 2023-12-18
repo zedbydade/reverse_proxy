bfa158b9e822394682244115a3b4e305f7a2be6d
Francisco Paradella <franciscoleite.dev@protonmail.com>
2023-12-18 20:09:24 -0300

    Add response to tcp_server

--- utils/tcp_server.rb
+++ utils/tcp_server.rb
@@ -1,12 +1,18 @@
 #!/usr/bin/ruby
 
 require 'socket'
-
-server = TCPServer.new('localhost', 2000) # Server bind to port 2000
+server = TCPServer.new('localhost', 1111) # Server bind to port 2000
 loop do
   p "Starting server: #{server.inspect}"
   client = server.accept # Wait for a client to connect
   p "With client: #{client.inspect}"
-  client.write 'TU-TU-RU'
+
+  response = "HTTP/1.1 200 OK\r\n" +
+    "Content-Type: text/plain\r\n" +
+    "Connection: close\r\n" +
+    "\r\n" +
+    "TU-TU-RU!"
+
+  client.puts(response)
   client.close
 end
